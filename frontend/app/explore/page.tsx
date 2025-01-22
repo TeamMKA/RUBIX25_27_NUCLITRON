f8b296979e2d77968097fd6fabfb9a9775b3edca
Mustafa <yusufmustufa@gmail.com>
2025-01-23 02:53:05 +0530

    Explore section Completed

---
+++ frontend/app/explore/page.tsx
@@ -0,0 +1,12 @@
+import Display from "@/components/Display";
+import React from "react";
+
+const Explore = () => {
+  return (
+    <main className="relative bg-black-100 flex justify-center items-center flex-col mx-auto  overflow-clip">
+        <Display />
+    </main>
+  );
+};
+
+export default Explore;
